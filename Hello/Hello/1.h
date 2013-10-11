cbfe6a8a4bae5e7920c93d1880f4e223b95e5014
amareshkv <amaresh.kv@demansol.com>
2013-10-11 14:03:46 +0530

    updated code

---
+++ Hello/Hello/1.h
@@ -0,0 +1,13 @@
+//
+//  1.h
+//  Hello
+//
+//  Created by Amaresh KV on 11/10/13.
+//  Copyright (c) 2013 Amaresh KV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface _ : UIViewController
+
+@end
---
+++ Hello/Hello/1.m
@@ -0,0 +1,38 @@
+//
+//  1.m
+//  Hello
+//
+//  Created by Amaresh KV on 11/10/13.
+//  Copyright (c) 2013 Amaresh KV. All rights reserved.
+//
+
+#import "1.h"
+
+@interface _ ()
+
+@end
+
+@implementation _
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
